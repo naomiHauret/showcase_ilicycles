9d54dff98b26bab751c6aafd44e572dc1a9d0535
Naomi Hauret <naomi@troopers.email>
2019-06-20 10:26:09 +0200

    :rocket:

--- utils/config.dist.js
+++ utils/config.dist.js
@@ -1,8 +1,8 @@
 export const CONTENT_API_URL = "https://ilicycleswebsite.cdn.prismic.io/api/v2"
 
-export const MAIL_TARGET = "michel@ilicycles.com"
+export const MAIL_TARGET = ""
 
-export const MAILER_KEY = "bnjhfxj8"
+export const MAILER_KEY = ""
 export const AVAILABLE_LOCALES = {
   en: "en_gb",
   fr: "fr_fr",
